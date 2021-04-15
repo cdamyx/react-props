023f4c764288ddb730608b81ccdca3c1533b9f68
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2021-04-14 20:52:28 -0500

    added some props

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
-
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
+    <Card
+      name="Beyonce"
+      tel="+123 456 789"
+      email="b@beyonce.com"
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
     />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
-
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
+    <Card
+      name="Butts"
+      tel="5555555555"
+      email="joe@microsoft.com"
+      img="https://www.gannett-cdn.com/-mm-/d45a1bc902cb3f4367b332e27f859c7252d4b7fa/c=0-109-2119-1306/local/-/media/2017/03/02/USATODAY/USATODAY/636240538720039138-GettyImages-509934328.jpg?width=3200&height=1680&fit=crop"
     />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
   </div>,
   document.getElementById("root")
 );
